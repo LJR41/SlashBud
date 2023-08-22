0ca366da32258677190ed8dd8f7720ec07451a63
Kye Clevenger <Kyeclevenger@gmail.com>
2023-08-22 15:13:39 -0600

    kyes_stuff

--- client/slashbud/src/App.js
+++ client/slashbud/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
+import HomePage from './views/HomePage';
+// import { CarouselWithContent } from './CarouselWithContent'
 
 function App() {
   return (
     <div className="App">
       <p>First Merge Commit</p>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
