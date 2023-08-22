57e48f42d57aa50d6154cd9a0d5e3c3f8acaa4d9
Kye Clevenger <Kyeclevenger@gmail.com>
2023-08-22 15:21:03 -0600

    howdy

--- client/slashbud/src/views/CarouselWithContent.js
+++ client/slashbud/src/views/CarouselWithContent.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
+// howdy
+
 export function CarouselWithContent() {
     const [games, setGames] = useState([])
     const { id } = useParams()
