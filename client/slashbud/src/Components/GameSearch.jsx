fa1ecec6c7c03eed49b26e02170a6dd34d1aa70e
Kye Clevenger <Kyeclevenger@gmail.com>
2023-08-24 14:44:24 -0600

    css done

--- client/slashbud/src/Components/GameSearch.jsx
+++ client/slashbud/src/Components/GameSearch.jsx
@@ -85,58 +85,70 @@ const GameSearch = () => {
 
 
     return (
-        <div>
-            <div >
-                <form onSubmit={handleSubmit}>
-                    <button type="submit">Search</button>
-                    <input type="text" placeholder="Type here..." value={gameSearch} onChange={e => { setGameSearch(e.target.value) }} />
-                    <select name="genre" id="genre">
-                        <option hidden>Genre</option>
-                        <option value="FPS">FPS</option>
-                        <option value="Horror">Horror</option>
-                        <option value="Action">Action</option>
-                        <option value="Simulation">Simulation</option>
-                    </select>
-                    <select name="platform" id="platform">
-                        <option hidden>Platform</option>
-                        <option value="PC">PC</option>
-                        <option value="Playstation">Playstation</option>
-                        <option value="Xbox">Xbox</option>
-                        <option value="Switch">Switch</option>
-                    </select>
-                </form>
+        <div className="flex-col bg-gradient-to-r from-cyan-600 to-purple-500 ... space ">
+            <div className="move ...ring-offset-2 ring-4 items-center justify-center max-w-lg p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <h5 className=" m-b-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Search For A Game :D</h5>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder=" Type here..." className='rounded-lg text-center ring-offset-3 ring-2' value={gameSearch} onChange={e => { setGameSearch(e.target.value) }} />
+                        <select name="genre " id="genre" className='rounded-lg m-1'>
+                            <option hidden>Genre</option>
+                            <option value="FPS">FPS</option>
+                            <option value="Horror">Horror</option>
+                            <option value="Action">Action</option>
+                            <option value="Simulation">Simulation</option>
+                        </select>
+                        <select name="platform" id="platform" className='rounded-lg m-1'>
+                            <option hidden>Platform</option>
+                            <option value="PC">PC</option>
+                            <option value="Playstation">Playstation</option>
+                            <option value="Xbox">Xbox</option>
+                            <option value="Switch">Switch</option>
+                        </select>
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-.5 px-2 rounded-full m-1">Search</button>
+                    </form>
+                </div>
+                <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', flexWrap: 'wrap' }}>
+                    <table class="border-separate border-spacing-2 border border-slate-500 ...">
+                        <thead>
+                            <tr>
+                                <th class="border border-slate-600 ...">Title</th>
+                                <th class="border border-slate-600 ...">Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody className="mb-12 size">
+                            {foundGame ?
+                                foundGame.map((eachGame, Idx) => {
+                                    return (
+                                        <tr>
+                                            <td class="border border-slate-700 ..."> {eachGame.name}</td>
+                                            <td class="border border-slate-700 ..." >{eachGame.rating ? Math.round(eachGame.rating) : "N/A"}</td>
+                                        </tr>
+                                    )
+                                })
+                                : <tr></tr>}</tbody>
+                    </table>
+                </div>
             </div>
-            <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', flexWrap: 'wrap' }}>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>
-                                Title
-                            </th>
-                            <th>
-                                Rating
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody >
-                        {foundGame ?
-                            foundGame.map((eachGame, Idx) => {
-                                return (
-                                    <tr>
-                                        <td>
-                                            {eachGame.name}
-                                        </td>
-                                        <td>
-                                            { eachGame.rating?Math.round(eachGame.rating): "N/A"
-                                            }
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                            : <tr>Search for a Game</tr>}</tbody>
-                </table>
 
-            </div>
+            <table>
+                <tbody className="mb-12 size">
+                    {foundGame ?
+                        foundGame.map((eachGame, Idx) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        {eachGame.name}
+                                    </td>
+                                    <td>
+                                        {eachGame.rating ? Math.round(eachGame.rating) : "N/A"
+                                        }
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        : <tr></tr>}</tbody>
+            </table>
             <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center', flexWrap: 'wrap' }}>
                 <table >
                     <thead>
@@ -176,7 +188,7 @@ const GameSearch = () => {
                     </tbody>
                 </table>
             </div>
-        </div>
+        </div >
     )
 }
 
