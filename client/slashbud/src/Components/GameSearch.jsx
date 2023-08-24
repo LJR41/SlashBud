94c0d287c43fa3194a6d6044e1566db3ac9377f1
daisytorres <daisytorres.cal@gmail.com>
2023-08-24 14:50:16 -0700

    updated list ID on all_lists

--- client/slashbud/src/Components/GameSearch.jsx
+++ client/slashbud/src/Components/GameSearch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import NavBar from './NavBar'
 
 const GameSearch = () => {
