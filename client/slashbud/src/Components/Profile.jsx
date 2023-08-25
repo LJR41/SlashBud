57db7285ff3e658a63a6554e1a399c98df30a443
Anthonylooby1 <anthonylooby@yahoo.com>
2023-08-25 11:38:23 -0400

    started profile, doesnt look great atm

--- client/slashbud/src/Components/Profile.jsx
+++ client/slashbud/src/Components/Profile.jsx
@@ -6,44 +6,45 @@ import { useParams } from 'react-router-dom'
 const Profile = () => {
 
     const [oneUser, setOneUser] = useState([])
+    const [listId, setListId] = useState([])
     const { id } = useParams()
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/lists/${id}`)
-        .then(res => {
-            setOneUser(res.data)
-            console.log(res)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then(res => {
+                setOneUser(res.data)
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
 
-  return (
-    <div>
-        <NavBar />
-        <div className='flex ... border-solid border-2 border-black ...'>
-            <div>
-                <h1>{oneUser.firstName}</h1>
+    return (
+        <div>
+            <NavBar />
+            <div className='flex ... border-solid border-2 border-black ...'>
+                <div>
+                    <h1>{oneUser.firstName}</h1>
+                </div>
+                <div>
+                    <h1>{oneUser.firstName}</h1>
+                </div>
+                <div >
+                    <h1>{oneUser.firstName}</h1>
+                </div>
             </div>
-            <div>
-                <h1>{oneUser.firstName}</h1>
+            <div className='flex ... border-solid border-2 border-black ...'>
+                <div>
+                    <h1>{oneUser.lastName}</h1>
+                </div>
             </div>
-            <div >
-                <h1>{oneUser.firstName}</h1>
+            <div className='flex ... border-solid border-2 border-black ...'>
+                <h1>{oneUser.displayName}</h1>
             </div>
         </div>
-        <div className='flex ... border-solid border-2 border-black ...'>
-            <div>
-                <h1>{oneUser.lastName}</h1>
-            </div>
-        </div>
-        <div className='flex ... border-solid border-2 border-black ...'>
-            <h1>{oneUser.displayName}</h1>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Profile
